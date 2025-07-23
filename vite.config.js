d7d44dfaaec88cdf0196e42bf6f9d31cec4ba9da
unknown <danibanez.x@gmail.com>
2025-07-23 18:35:27 +0800

    update docker compose

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: process.env.VITE_PORT || 3000,
-    host: process.env.VITE_HOST || 'localhost',
-    allowedHosts: ['danibanez.publicvm.com']
+    host: process.env.VITE_HOST || 'localhost'
+    // allowedHosts: ['danibanez.publicvm.com']
   },
   preview: {
     port: process.env.VITE_PORT || 3000,
-    host: process.env.VITE_HOST || '0.0.0.0',
-    allowedHosts: ['danibanez.publicvm.com']
+    host: process.env.VITE_HOST || '0.0.0.0'
+    // allowedHosts: ['danibanez.publicvm.com']
   }
 });
